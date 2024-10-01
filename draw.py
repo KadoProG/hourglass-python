14ba0e5454761ed42c237ba3900864e6d208c845
KadoProG <splatrain@gmail.com>
2024-10-01 12:32:37 +0900

    refactor プライベートメソッドとして定義

--- draw.py
+++ draw.py
@@ -24,10 +24,10 @@ class Draw:
     _pre_angle = INIT_ANGLE
 
     def __init__(self, stdscr):
-        self.stdscr = stdscr
+        self._stdscr = stdscr
         # 非エコーモードに設定
         curses.noecho()
-        self.stdscr.nodelay(True)
+        self._stdscr.nodelay(True)
 
         # 色の初期化
         curses.start_color()
@@ -36,7 +36,7 @@ class Draw:
         )  # カウンターの数字を赤色に設定
 
     def draw_routine(self, frame_count: int, ball_count: int):
-        self.stdscr.clear()
+        self._stdscr.clear()
         # 空のグリッドを作成
         self._grid = [["◯" for _ in range(GRID_SIZE)] for _ in range(GRID_SIZE * 2)]
 
@@ -45,43 +45,43 @@ class Draw:
 
         # １行目のメッセージ
         row0_message = f"frame: {frame_count:> 5} ball: {ball_count:> 3} angle: {angle[0]:> 4}° GRID_SIZE: {GRID_SIZE:< 3}"
-        self.stdscr.addstr(0, 0, row0_message)
+        self._stdscr.addstr(0, 0, row0_message)
 
         # ２行目のメッセージ
         if self._pre_angle != angle[0] or self._count_angle_diff_frame < 1 / FRAMERATE:
             if self._pre_angle != angle[0]:
                 self._pre_angle = angle[0]
                 self._count_angle_diff_frame = 0
-            self.stdscr.addstr(1, 4, "[")
-            self.stdscr.addstr("keypress", curses.color_pair(1))
-            self.stdscr.addstr(f"] angleを{angle[0]:> 4}° に変更")
+            self._stdscr.addstr(1, 4, "[")
+            self._stdscr.addstr("keypress", curses.color_pair(1))
+            self._stdscr.addstr(f"] angleを{angle[0]:> 4}° に変更")
             self._count_angle_diff_frame += 1
 
         # グリッドを描写
         for index, row in enumerate(self._grid):
             row_text = ("  " * GRID_SIZE if index >= GRID_SIZE else "") + " ".join(row)
-            self.stdscr.addstr(index + 2, 2, row_text)
+            self._stdscr.addstr(index + 2, 2, row_text)
 
         # これは数字を表示するための行
         row_text = ("").join([f"{i:> 2}" for i in range(GRID_SIZE)])[1:]
-        self.stdscr.addstr(
+        self._stdscr.addstr(
             GRID_SIZE * 2 + 2, 0 + 2, f"{row_text} {row_text}", curses.color_pair(1)
         )
 
         # これは数字を表示するための列
         for index in range(GRID_SIZE * 2):
-            self.stdscr.addstr(
+            self._stdscr.addstr(
                 index + 2, GRID_SIZE * 4 + 1, f"{index:> 2}", curses.color_pair(1)
             )
 
         for index, log in enumerate(self._logs):
-            self.stdscr.addstr(
+            self._stdscr.addstr(
                 GRID_SIZE * 2 + 5 + index, 0, "[" + str(index + 1) + "]" + log
             )
 
             # アラームを表示
         if is_finish_falling[0]:
-            self.stdscr.addstr(1, GRID_SIZE * 4 + 4, "Alerm!")
+            self._stdscr.addstr(1, GRID_SIZE * 4 + 4, "Alerm!")
 
     def _draw_grid(self, balls, index: int):
         # balls配列に基づいてドットを配置
