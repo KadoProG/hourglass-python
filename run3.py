9198fba9cf91b11491000d0238eececc391f37a7
KadoProG <splatrain@gmail.com>
2024-10-17 05:39:03 +0900

    update センサーの値を適用

--- run3.py
+++ run3.py
@@ -5,9 +5,17 @@ from app.sound import sound as Sound
 import pygame
 import sys
 import math
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+boot = os.getenv("BOOT")
+sensor = os.getenv("SENSOR")
 
 
 def main():
+    is_fixed = False
     # ここで使用される変数の初期化
     angle = INIT_ANGLE
     pre_is_finish_falling = True
@@ -50,6 +58,13 @@ def main():
                     angle += 90
             elif event.type == pygame.VIDEORESIZE:
                 drawPygame.video_resize(event)
+
+        # ラズパイのセンサーがある場合はセンサーの値を取得
+        if not is_fixed and boot == "raspberrypi" and sensor == "true":
+            from app.events.mpu_events import get_mpu_angle
+
+            roll, _ = get_mpu_angle()
+            angle = -roll + 45
 
         # 角度を更新
         angle += auto_rotation**2 * (-1 if auto_rotation < 0 else 1)
